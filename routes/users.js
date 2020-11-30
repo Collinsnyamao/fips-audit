70d7ab8af478d68c434aae2a085db6536a01cff0
collins nyamao <collins.nyamao@strathmore.edu>
2020-11-30 11:11:57 +0300

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  res.send('new user is ' + username + '\n' + 'password is ' + password);
+});
+
+module.exports = router;
